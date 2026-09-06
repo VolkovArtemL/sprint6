2b88ef8de48a98fe520849171de8508939ae0bdd
Artem Volkov <tem99volk@gmail.com>
2026-09-06 19:37:42 +0300

    1

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "MORSE-SERVER: ", log.Ldate|log.Ltime)
+
+	srv := server.New(logger)
+
+	logger.Println("сервер запущен на порту 8080")
+	if err := srv.ListenAndServe(); err != nil {
+		logger.Fatal(err)
+	}
 }
